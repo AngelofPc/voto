70e97db34d7e821756598dbd861b6fe1c05aafbe
Jurshsmith <jurshsmith@gmail.com>
2018-06-29 23:08:28 +0100

    'JJ

--- includes/head.php
+++ includes/head.php
@@ -19,7 +19,7 @@
         <!-- end script -->
 
         <!--css-->
-        <link rel = "stylesheet" href = "assets/mystyle.css">
+        <link rel = "stylesheet" href = "assets/css/main.css">
         <link rel = "stylesheet" href = "assets/bootstrap.min.css">
         <!-- end css -->
 
@@ -27,7 +27,7 @@
   <body>
 
 
-<nav class="navbar navbar-inverse">
+<nav class="navbar j-navbar">
   <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -35,18 +35,14 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span> 
       </button>
-      <a class="navbar-brand" href="#">WebSiteName</a>
+     <img src = "images/voto.png" style = "height : 50px">
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">Page 1</a></li>
-        <li><a href="#">Page 2</a></li> 
-        <li><a href="#">Page 3</a></li> 
-      </ul>
+      
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> About</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Get Started</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Sign In</a></li>
       </ul>
     </div>
   </div>
