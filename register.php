315782e658fb66daf71c5fdb592ebf3c5b288ca7
O-Temitayo <ogunsusitemitayo99@gmail.com>
2018-06-29 20:30:49 +0100

    main css

--- register.php
+++ register.php
@@ -4,4 +4,11 @@
  * User: AngelofPc
  * Date: 6/29/2018
  * Time: 7:41 PM
- */
+ */?>
+<form action="register.php" method="post">
+    <div class="form-group">
+    <input type="text" name="name" id="name" class="form-control" value="<?= $name;?>">
+    <input type="text" name="email" id="email" class="form-control" value="<?= $eamil;?>">
+    <input type="text" name="password" id="password" class="form-control" value="<?= $password;?>">
+    </div>
+</form>
